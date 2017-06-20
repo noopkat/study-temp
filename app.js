3a0ace46bcb832e73f123922cfbbde915d7fb02f
Suz Hinton <suz.hinton@gmail.com>
2017-06-19 20:29:27 -0400

    server and client setup

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const skateboard = require('skateboard');
+const startReceiver = require('./lib/receiver');
+
+const setUpSocket = function(hubListener) {
+  skateboard({
+    dir: __dirname + '/public',         
+    port : process.env.PORT || 3000,                        
+    transports: ['polling', 'websocket']
+  }, function(stream) {
+    console.log('skateboard connected');
+    listenForDeviceData(stream, hubListener);
+  });
+}
+
+const listenForDeviceData = function(stream, hubListener) {
+  hubListener.on('message', (eventData) => {
+    const from = eventData.annotations['iothub-connection-device-id'];
+    if (from === process.env.IOT_DEVICE_ID) {
+      const jsonData = JSON.stringify(eventData.body);
+      console.log('Message Received: ' + jsonData);
+      stream.write(jsonData);
+    }
+  });
+};
+
+startReceiver().then(hubListener => setUpSocket(hubListener));
